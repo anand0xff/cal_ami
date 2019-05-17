c66c813faa7bf1957543e7e4702fcfd25abd85e0
Jo Taylor <jotaylor@stsci.edu>
2019-05-17 12:05:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Template for all NIRISS repos
+
+Author(s): Jo Taylor
+
+## What is the purpose of this repo?
+
+Perhaps it's to create a simulated observation, or maybe to analyze a certain type of data. Maybe this 
+is code to test pipeline steps. You should describe what the purpose of your new repo is here. 
+
+## Details
+
+Here's exactly how my code works: It takes an input file `myinput.fits` and performs the following
+corrections:
+* Correction 1
+* This is just to show you how to use bullets
+
+## How do you use this repo?
+
+Put some instructions on how to run your code here, like so. From the command line:
+```
+python setup.py install
+python runmycode.py --input_arg
+```
+
+Now look at these amazing output results your code produced!
+
+For more markdown tips, check out the official [Markdown guide](https://www.markdownguide.org/cheat-sheet/). 
+:sparkles: Happy version-controlling! :sparkles: 
---
+++ setup.py
@@ -0,0 +1,47 @@
+# For info on how to write a setup.py file, check out the link below 
+# or ask a friendly neighborhood python programmer! 
+# https://docs.python.org/3.7/distutils/setupscript.html
+
+# You can also alternatively use a requirements.txt file which contains ALL 
+# packages needed for your package. Ask someone about this before doing it!
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "mypackagename",
+    version = "0.0.1",
+    description = "My package does this really cool thing",
+    author = "Jo Taylor",
+    author_email = "jotaylor@stsci.edu",
+    keywords = ["astronomy", "jwst", "simulations", "etc"],
+    classifiers = ['Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Development Status :: 1 - Planning',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+	packages = find_packages(),
+    # Specify necessary, but minimal, requirements needed to run. 
+    # Only specify version numbers if you know some versions will break your code.
+    # An example of how to make another git repo a dependency of your package:
+    install_requires = ["setuptools",
+                        "numpy",
+                        "astropy"],
+#   This is just to show you how to specify version numbers
+#                        "matplotlib>=3.0.0,<3.1.0",
+#                        "beautifulsoup4>=1.0",
+    # Maybe you need access to a script in another repo for your package. Here's how you do that:
+#                        "package2"],
+#    dependency_links = ["git+ssh://git@grit.stsci.edu/NIRISS/package2.git#egg=catz-0.0.1"],
+    # If you need something from github, any of the below formats work:
+#    dependency_links = ["git+ssh://git@github.com/npirzkal/GRISMCONF.git#egg=GRISMCONF-0.1"],
+#    dependency_links = ["git+git@github.com:npirzkal/NIRCAM_Gsim@master#egg=NIRCAM_Gsim-0.1"],
+#    dependency_links = ["git+https://github.com/npirzkal/NIRCAM_Gsim@master#egg=NIRCAM_Gsim-0.1"],
+    
+#   Can also include entry_points, but you likely won't need to do this.
+#   entry_points = {"console_scripts": [
+#                       "dothething = mypackagename.package:module"],
+#                    },                       
+    )
+
